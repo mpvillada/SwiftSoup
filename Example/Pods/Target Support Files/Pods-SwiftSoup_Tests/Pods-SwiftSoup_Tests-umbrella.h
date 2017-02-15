95337d6c86922a85df36b55a3370e90d245022c5
mpvillada <mpvillada@gmail.com>
2017-02-15 20:05:23 +0100

    Removed unused files

--- Example/Pods/Target Support Files/Pods-SwiftSoup_Tests/Pods-SwiftSoup_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_SwiftSoup_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwiftSoup_TestsVersionString[];
-
